ee56b6760bf6fe3b33eb83febe5cfa503f1869aa
valleykid <valleykiddy@gmail.com>
2022-05-29 19:50:23 +0800

    refactor: same as online-example

--- use-app-with-lifecycles/index.js
+++ use-app-with-lifecycles/index.js
@@ -1,5 +1,12 @@
 // @ts-check
-import { register, start, use, HistoryType, MidwareName, FileType } from '@satumjs/core';
+import {
+  register,
+  start,
+  use,
+  HistoryType,
+  MidwareName,
+  FileType,
+} from '@satumjs/core';
 import { simpleSandboxMidware } from '@satumjs/simple-midwares';
 import midwareSingleSpa from '@satumjs/midware-single-spa';
 
@@ -19,14 +26,18 @@ use((sys, microApps, next) => {
   sys.set(MidwareName.code, (code, file) => {
     const ext = sys.fileExtName(file);
     if (ext === FileType.CSS) {
-      const { assetPublicPath = entry } = microApps.find(item => item.fileExtNameMap[file]) || {};
-      const matchs = code.match(/url\([^\)]+\)/ig);
+      const { assetPublicPath = entry } =
+        microApps.find((item) => item.fileExtNameMap[file]) || {};
+      const matchs = code.match(/url\([^\)]+\)/gi);
       if (matchs) {
-        matchs.forEach(s => {
+        matchs.forEach((s) => {
           const paths = s.match(/\(([^\)]+)\)/);
           const url = paths ? paths[1] : undefined;
           if (url && url.charAt(0) === '.') {
-            code = code.replace(s, s.replace(url, assetPublicPath + url.replace(/^\.{1,2}\//, '')));
+            code = code.replace(
+              s,
+              s.replace(url, assetPublicPath + url.replace(/^\.{1,2}\//, ''))
+            );
           }
         });
       }
@@ -38,15 +49,24 @@ use((sys, microApps, next) => {
 });
 
 use(simpleSandboxMidware, {
-  winVariable(key) {
+  winVariable(key, fakeWin, win) {
     // 设置这几个变量，模拟 micro-app 环境，因为借用的他们的微应用。
     // 这样，应用执行时会导出 bootstrap/mount/unmount
     if (key === '__MICRO_APP_ENVIRONMENT__') return true;
     if (key === '__MICRO_APP_PUBLIC_PATH__') return entry;
     if (key === '__MICRO_APP_NAME__') return 'vue2';
-  }
+
+    if (key === 'getComputedStyle') {
+      const orginGetComputedStyle = win[key].bind(win);
+      return (...args) => {
+        let el = args.shift();
+        el = el instanceof Document ? el.body : el;
+        return orginGetComputedStyle(el, ...args);
+      };
+    }
+  },
 });
 
 use(midwareSingleSpa);
 
-start({ baseHistoryType: HistoryType.HASH });
+start({ hostHistoryType: HistoryType.HASH });
