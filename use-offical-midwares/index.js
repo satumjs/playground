9b8849cc001edd60fc914c577139de4528df115a
valleykid <valleykiddy@gmail.com>
2022-05-29 19:54:12 +0800

    refactor: same as use-offical-midwares example

--- use-offical-midwares/index.js
+++ use-offical-midwares/index.js
@@ -1,4 +1,11 @@
-import { MidwareName, HistoryType, register, start, use, corsRuleLabel } from '@satumjs/core';
+import {
+  MidwareName,
+  HistoryType,
+  register,
+  start,
+  use,
+  corsRuleLabel,
+} from '@satumjs/core';
 import { simpleSandboxMidware } from '@satumjs/simple-midwares';
 import midwareSingleSpa from '@satumjs/midware-single-spa';
 
@@ -8,7 +15,7 @@ register({
   history: HistoryType.HASH,
   rules: {
     rule: '/',
-    container: '#mountNode'
+    container: '#mountNode',
   },
 });
 
@@ -16,7 +23,7 @@ use((system, _, next) => {
   // 设置跨域服务，使用中间件节点 MidwareName.urlOption
   // 该节点设置类型请参考 https://satumjs.github.io/website/guide/midware/flow-nodes.html#%E6%8C%89%E6%94%AF%E6%8C%81%E3%80%8E%E5%8D%95%E4%B8%AA-%E5%A4%9A%E4%B8%AA%E3%80%8F%E5%88%92%E5%88%86
   system.set(MidwareName.urlOption, {
-    corsRule: `https://vklife.fun/proxy?target=${corsRuleLabel}`
+    corsRule: `https://vklife.fun/proxy?target=${corsRuleLabel}`,
   });
   next();
 });
@@ -27,4 +34,4 @@ use(simpleSandboxMidware);
 // 使用 single-spa 中间件处理微应用的加载/卸载
 use(midwareSingleSpa);
 
-start({ baseHistoryType: HistoryType.HASH });
+start({ hostHistoryType: HistoryType.HASH });
