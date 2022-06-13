638afa93f4e9724c62e5c4f5687bfbb8fcc53984
valleykid <valleykiddy@gmail.com>
2022-06-13 17:41:02 +0800

    fix: remove some useless codes

--- simple-share-third-library/index.js
+++ simple-share-third-library/index.js
@@ -13,7 +13,7 @@ register([{
   entry: 'http://localhost:3001',
   rules: {
     rule: '/app1',
-    container: '#root',
+    container: '#rootFromLayout',
     layout: '/layout'
   },
   shareProps: {
@@ -26,18 +26,12 @@ register([{
   entry: 'http://localhost:3002',
   rules: {
     rule: '/app2',
-    container: '#root',
+    container: '#rootFromLayout',
     layout: '/layout'
   }
 }]);
 
-use(simpleSandboxMidware, {
-  winVariable(k, fakeWin) {
-    // window 上的方法在沙箱运行时，会被 .bind 锁定上下文
-    // jQuery 不用被 bind，在默认处理前返回
-    if (k === 'jQuery') return fakeWin[k];
-  }
-});
+use(simpleSandboxMidware);
 use(mountNodeMidware);
 
 start({
